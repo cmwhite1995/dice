5e8f181fdf9fef4ed1ec0ba7905306e40b7a8650
christopher white <cmwhite1995@gmail.com>
2019-09-10 07:20:15 -0400

    UI PROTOTYPE

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var blah: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        blah = Int.random(in: 0...5)
+        print(blah)
     }
+    
+    
 
 
 }

a1e1f42482d12c2476447161b0b90f3d41db4d08
christopher white <cmwhite1995@gmail.com>
2019-09-10 02:05:33 -0400

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by christopher white on 9/10/19.
+//  Copyright © 2019 christopher white. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
